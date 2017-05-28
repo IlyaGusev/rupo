45049a4f591075476d71761bf2319d754376e847
Daniil Anastasev <DaAnastasev@abbyy.ru>
2017-05-28 21:37:51 +0300

    Поправил Lstm

--- rupo/generate/lstm.py
+++ rupo/generate/lstm.py
@@ -6,21 +6,19 @@ from keras.models import Model, Sequential
 from keras.layers import Input, Activation, Embedding, TimeDistributed, Dense, Dropout, Reshape, Merge, Highway, \
                          LSTM, Convolution2D, MaxPooling2D, BatchNormalization, SpatialDropout1D, Masking
 from keras.callbacks import Callback
-from keras.models import model_from_json, load_model
-from rupo.generate.lemmatized_vocabulary import LemmatizedVocabulary, LemmatizedWord
+from keras.models import model_from_json
+from rupo.generate import lemmatized_vocabulary
 
 class LSTM_Container(object):
     def __init__(self, path : str):
-        # json_string = open(path + '.json', 'r', encoding='utf8').readline()
-        # self.model = model_from_json(json_string)
-        # self.model.load_weights(path + '_weights.h5')
-        self.model = load_model(path + '.model')
+        json_string = open(path + '.json', 'r', encoding='utf8').readline()
+        self.model = model_from_json(json_string)
+        self.model.load_weights(path + '_weights.h5')
         self.lemmatized_vocabulary = pickle.load( open(path + "Voc.pkl", "rb") )
         self.num_of_words = len(self.lemmatized_vocabulary.lemmatizedWords)
         self.index2tags_vector = pickle.load( open(path + "_index2tags_vector.pkl", "rb") )
         self.GRAMMEMES_COUNT = 54
         self.SOFTMAX_SIZE = 50000
-        print(self.model.summary())
 
     def __get_word_index(self, word):
         return min(self.lemmatized_vocabulary.get_word_form_index(word), self.SOFTMAX_SIZE)
@@ -35,3 +33,5 @@ class LSTM_Container(object):
             X_emb[0, ind] = self.__get_word_index(word)
             X_gr[0, ind] = self.index2tags_vector[word.gr_tag]
         return self.model.predict([X_emb, X_gr], verbose=0)[0]
+
+
