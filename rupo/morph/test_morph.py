8d690408c3029ddf7130a451fb8e28f47d2d9ffd
Ilya Gusev <phoenixilya@gmail.com>
2017-07-11 02:58:29 +0300

    Точно пофиксил тест.

--- rupo/morph/test_morph.py
+++ rupo/morph/test_morph.py
@@ -9,6 +9,6 @@ class TestMorphology(unittest.TestCase):
     def test_markov(self):
         input_filename = os.path.join(EXAMPLES_DIR, "text.txt")
         output_filename = os.path.join(EXAMPLES_DIR, "morph_markup.txt")
-        RussianMorphology.do_markup(input_filename, output_filename)
+        RussianMorphology.pymorphy_process(input_filename, output_filename)
         with open(output_filename, "r", encoding="utf-8") as f:
             self.assertEqual(f.readlines()[2], "жизни	жизнь	NOUN	Animacy=Inan|Case=Gen|Gender=Fem|Number=Sing\n")
